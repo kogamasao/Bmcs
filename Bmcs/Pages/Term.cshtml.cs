99fafefd88f8a133daa05f79609dd1fee45a66b3
kogamasao <proud.of.y.d@gmail.com>
2021-10-27 19:11:27 +0900

    Terms of Service And Privacy Policy Confirm Add

---
+++ Bmcs/Pages/Term.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Bmcs.Models;
+using Bmcs.Data;
+
+namespace Bmcs.Pages
+{
+    public class TermModel : PageModelBase<TermModel>
+    {
+        public TermModel(ILogger<TermModel> logger, BmcsContext context) : base(logger, context)
+        {
+
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Bmcs/wwwroot/js/user-account.js
@@ -0,0 +1,32 @@
+﻿$(function () {
+
+    //登録ボタン制御
+    IsEnableSubmit();
+
+    //登録ボタン制御
+    function IsEnableSubmit() {
+
+        var term = $("#term");
+        var privacy = $("#privacy");
+        var submit = $("#submit");
+
+        if (term.prop('checked') && privacy.prop('checked')) {
+            submit.prop("disabled", false);
+        }
+        else {
+            submit.prop("disabled", true);
+        }
+    }
+
+    //利用規約
+    $("#term").change(function () {
+        //登録ボタン制御
+        IsEnableSubmit();
+    });
+
+    //プライバシーポリシー
+    $("#privacy").change(function () {
+        //登録ボタン制御
+        IsEnableSubmit();
+    });
+});
